dc8702dcfb08c523179978dc2e35eff496d1679b
Anastasia <anastasia.lukina1602@icloud.com>
2023-03-11 00:01:55 +0400

    Add comments

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,6 @@ use App\Models\Order;
 use App\Models\Product;
 use App\Models\UsersProduct;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
@@ -65,6 +64,9 @@ class ProductController extends Controller
             'type' => $request->input('type'),
         ]);
 
+        //This is not good practice to modify orders in product controller,
+        //but since I don't have button to modify orders in this project and that was not the requirement,
+        //I will do it here.
         $orders = Order::all();
         foreach ($orders as $order) {
             if ($order->product_name == $product->name) {
